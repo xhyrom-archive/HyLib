74c2c990e395a2b573bca9f0d078edb47682ca30
xhyrom <generalkubo@gmail.com>
2022-12-03 15:08:04 +0100

    feat(BStatsManager): allow to register addon with custom bstats id

--- src/main/kotlin/me/xhyrom/hylib/api/managers/BStatsManager.kt
+++ src/main/kotlin/me/xhyrom/hylib/api/managers/BStatsManager.kt
@@ -1,19 +1,29 @@
 package me.xhyrom.hylib.api.managers
 
+import me.xhyrom.hylib.api.structs.Config
 import org.bstats.bukkit.Metrics
 import org.bstats.charts.AdvancedPie
 import org.bukkit.plugin.java.JavaPlugin
 
-class BStatsManager(plugin: JavaPlugin) {
-    private val bStats = Metrics(plugin, 16983)
+class BStatsManager(private val hyPlugin: JavaPlugin) {
+    private var bStats: Metrics? = null
     private val addons = arrayListOf<String>()
 
     fun registerAddon(plugin: JavaPlugin) {
         addons.add(plugin.name)
     }
 
+    fun registerAddon(plugin: JavaPlugin, id: Int, config: Config) {
+        addons.add(plugin.name)
+
+        if (config.getBoolean("send-metrics")) {
+            Metrics(plugin, id)
+        }
+    }
+
     fun register() {
-        bStats.addCustomChart(AdvancedPie("hylib_addons") {
+        bStats = Metrics(hyPlugin, 16983)
+        bStats?.addCustomChart(AdvancedPie("hylib_addons") {
             val valueMap = HashMap<String, Int>()
             for (addon in addons) {
                 valueMap[addon] = 1
