ce6a27438a75b65e4e561ac754b1681dc8a8a752
xhyrom <generalkubo@gmail.com>
2022-12-18 20:46:14 +0100

    feat: support for 1.19.3 packetevents bump

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -6,8 +6,8 @@ dependencies {
     api(project(":hylib-common"))
     api("org.bstats:bstats-bukkit:3.0.0")
     api("dev.jorel:commandapi-shade:8.7.0")
-    api("com.github.retrooper.packetevents:spigot:2.0.0-SNAPSHOT")
+    api("com.github.retrooper.packetevents:spigot:2.0.0-20221215.205426-101")
 
     compileOnly("org.jetbrains:annotations:23.0.0")
-    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
 }
