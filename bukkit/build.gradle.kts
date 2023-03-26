928c3c178d4084f056297bfcc889f1a5d84d36f8
xHyroM <generalkubo@gmail.com>
2023-03-26 12:28:28 +0200

    build(bukkit): upgrade packetevents

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     api(project(":hylib-common"))
     api("org.bstats:bstats-bukkit:3.0.1")
     api("dev.jorel:commandapi-shade:8.8.0")
-    api("com.github.retrooper.packetevents:spigot:2.0.0")
+    api("com.github.retrooper.packetevents:spigot:2.0.0-SNAPSHOT")
 
     compileOnly("org.jetbrains:annotations:24.0.1")
     compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
